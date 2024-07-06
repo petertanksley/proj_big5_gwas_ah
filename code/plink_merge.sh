a4828c218e496991c81e57e01a8c6e8220c2d7e7
Peter Tanksley <tankslpr@login1.ls6.tacc.utexas.edu>
2024-07-06 13:53:00 -0500

    redownloaded AH geno files

--- code/plink_merge.sh
+++ code/plink_merge.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 1              # Total # of nodes (must be 1 for serial)
 #SBATCH -n 1              # Total # of mpi tasks (should be 1 for serial)
 #SBATCH -p normal         # Queue (partition) name
-#SBATCH -t 24:00:00       # Run time (hh:mm:ss)
+#SBATCH -t 05:00:00       # Run time (hh:mm:ss)
 #SBATCH -A OTH21060       # Project/Allocation name (req'd if you have more than 1)
 #SBATCH --mail-type=all   # Send email at begin and end of job
 #SBATCH --mail-user=peter.tanksley@austin.utexas.edu
@@ -16,7 +16,7 @@
 export PATH=$PATH:/work/07624/tankslpr/ls6/TOOLS
 
 # Define input/output directories
-INPUT="../input/ah_genotypes/ah_imputed_set1_1kg"
+INPUT="../input/phg001099.v1.AddHealth.genotype-imputed-data.c1.GRU-IRB-PUB-GSO.set1"
 TEMP="../temp"
 
 # Create the temp directory if it doesn't exist
@@ -29,25 +29,46 @@ CHROMOSOMES=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22)
 # Convert VCF files to PLINK binary format
 #=======================================================================#
 
-# Create merge list for PLINK
-vcf_list="${TEMP}/vcf_list.txt"
-> "$vcf_list"
+# Create pgen_list file for PLINK
+pgen_list="${TEMP}/pgen_list.txt"
+> "$pgen_list"
+
+# Convert VCF files
 for CHR in "${CHROMOSOMES[@]}"; do
-    plink --vcf "${INPUT}/chr${CHR}.dbGaP.dose.vcf.gz" --make-bed --out "${TEMP}/chr${CHR}"
-    echo "${TEMP}/chr${CHR}" >> "$vcf_list"
+    vcf_file="${INPUT}/chr${CHR}.dbGaP.dose.vcf.gz"
+    pgen_file="${TEMP}/chr${CHR}.pgen"
+
+    if [ ! -f "$vcf_file" ]; then
+        mkdir -p "${INPUT}"
+        tar -zxvf "${INPUT}.tar.gz" "${vcf_file}"
+        if [ $? -ne 0 ]; then
+            echo "Error extracting ${vcf_file}"
+            exit 1
+        fi
+    fi
+
+    if [ ! -f "$pgen_file" ]; then
+        plink2 --vcf "$vcf_file" --make-pgen --out "${TEMP}/chr${CHR}"
+        if [ $? -ne 0 ]; then
+            echo "Error converting ${vcf_file} to PLINK format"
+            exit 1
+        fi
+    fi
+
+    echo "${TEMP}/chr${CHR}" >> "$pgen_list"
 done
 
 #=======================================================================#
-# Merge all binary files using PLINK (v1.9)
+# Merge all binary files using PLINK
 #=======================================================================#
 
 # Merge using merge list
-plink --merge-list "$vcf_list" --make-bed --out "${TEMP}/merged"
+plink2 --pmerge-list "$pgen_list" --make-pgen --merge-max-allele-ct 2 --out "${TEMP}/merged"
 
 #=======================================================================#
 # Filter to biallelic SNPs and convert to PLINK2 format with ACGT alleles using PLINK2
 #=======================================================================#
 
 # Filter to biallelic SNPs, convert to PLINK2 format, and filter to ACGT alleles
-plink2 --bfile "${TEMP}/merged" --biallelic-only strict --snps-only just-acgt --make-pgen --out "${TEMP}/ah_biallelic_acgt"
+plink2 --pfile "${TEMP}/merged" --max-alleles 2 --snps-only just-acgt --make-pgen --out "${TEMP}/ah_biallelic_acgt"
 
